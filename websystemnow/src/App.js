dd15ac6d495edc59b5f77f746b9ad946090bb7ef
ClaitonBarchet <claitonbarchet@live.com>
2024-04-15 21:08:52 -0300

    ajuste home

--- websystemnow/src/App.js
+++ websystemnow/src/App.js
@@ -1,8 +1,28 @@
+// geral
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-const App = () => {
+// hooks
+import { useState, useEffect } from "react";
+
+// pages
+import Login from "./pages/login/Login";
+
+function App () {
+
+  // AUTENTICAÇÃO DE USUÁRIO
+  const [user, setUser] = useState(undefined)
+
   return (
-    <div>App</div>
+    // https://www.w3schools.com/react/showreact.asp?filename=demo2_react_router
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+        <Route path="*" element={<Login />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- websystemnow/src/pages/login/Login.js
+++ websystemnow/src/pages/login/Login.js
@@ -2,7 +2,42 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div class="container">
+      <div class="row vh-100">
+        <form  id="login-form" action="home.html" class="form-login">
+          <div class="d-flex justify-content-center my-5 flex-column">
+            <div class="container">
+              <div class="text-cente mb-3">
+                {/* <img src="./assets/images/Logo ft black.png" alt="logo sinos" class="img-fluid" srcset=""> */}
+              </div>
+                                
+              <div class="mb-4">
+                <label for="exampleInputEmail1" class="form-label">E-mail</label>
+                  {/* <input type="email" class="form-control" id="email-login-input" aria-describedby="emailHelp"> */}
+                <div id="emailHelp" class="form-text">Utilize seu e-mail para realizar o login.</div>
+              </div>
+              <div class="mb-4">
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+                  {/* <input type="password" class="form-control" id="password-login-input"> */}
+              </div>
+              <div class="mb-4 form-check">
+                  {/* <input type="checkbox" class="form-check-input" id="exampleCheck1"> */}
+                <label class="form-check-label" for="exampleCheck1">Permanecer logado.</label>
+              </div>
+              <button type="module" class="btn button-login">Entrar</button>
+                                
+              <div class="row">
+                <div class="col">
+                  <p class="text-center mt-2 form-text" data-bs-toggle="modal"data-bs-target="#register-modal">Ainda não tem conta? 
+                  <span class="link-default">Criar conta</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
