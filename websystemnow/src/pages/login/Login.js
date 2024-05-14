dd0c0e368c0e7b95c58579a088c15e65d5abbc37
ClaitonBarchet <claitonbarchet@live.com>
2024-05-14 20:45:51 -0300

    ajuste css form

--- websystemnow/src/pages/login/Login.js
+++ websystemnow/src/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import styles from "../login/Login.css";
 import imagens from "../../imagens/Logo ft black.png";
 import Modal from 'react-modal';
@@ -18,48 +18,33 @@ Modal.setAppElement("#root");
     }
 
   return (
-    // <div className={styles.Login} class="container">
-    // <div className="Container">
-    <div>
       <body>
       <div class="row vh-100">
         <form  id="login-form" action="home.html" class="form-login">
+          
           <div class="d-flex justify-content-center my-5 flex-column">
-            <div class="container">
-              <div class="text-cente mb-3">
-              {/* https://pt.stackoverflow.com/questions/309953/como-inserir-imagem-usando-react-js */}
-                <img src={imagens} alt="logo sinos"></img>
-              </div>
-           
-              <div class="mb-4">
-                <label for="exampleInputEmail1">E-mail:</label>
-                <br></br>
-                  <input type="email" class="input-basic" id="email-login-input" aria-describedby="emailHelp"/>
-                <div id="emailHelp" class="form-text">Utilize seu e-mail para realizar o login.</div>
-              </div>
-              <p></p>
-              <div class="mb-4">
-                <label for="exampleInputPassword1">Senha:</label>
-                <br></br>
-                  <input type="password" class="input-basic" id="password-login-input"/>
-              </div>
-              <div class="mb-4 form-check">
-                  {/* <input type="checkbox" class="form-check-input" id="exampleCheck1"/> */}
-                {/* <label class="form-check-label" for="exampleCheck1">Permanecer logado.</label> */}
-              </div>
-                                
-              <div class="row">
-                <div class="col">
-                </div>
-              </div>
-            </div>
+            {/* https://pt.stackoverflow.com/questions/309953/como-inserir-imagem-usando-react-js */}
+            <img src={imagens} alt="logo sinos"></img>
           </div>
+
+            <div align="left" class="mb-3">E-mail:</div>
+              <div align="center">
+              <input type="email" class="input-basic" id="email-login-input"/>
+              </div>
+            <div align="left" class="mb-3">Utilize seu e-mail para realizar o login.</div>
+            <br></br>
+            <div align="left" class="mb-3" for="exampleInputPassword1">Senha:</div>
+            <div align="center">
+            <input type="password" class="input-basic" id="password-login-input"/>
+            </div>
         </form>
-        <p></p>
-        <button type="module" class="btn button-basic">Entrar</button>
-        <p></p>
-        <button class="btn button-criar-conta" onClick={openModal}>Crie sua conta aqui</button>
-      
+
+          <div align="center">
+            <p></p>
+            <button type="module" class="btn button-basic">Entrar</button>
+            <p></p>
+            <button class="btn button-criar-conta" onClick={openModal}>Crie sua conta aqui</button>
+          </div>
       </div>
 
         <div className = "modal"> 
@@ -71,9 +56,8 @@ Modal.setAppElement("#root");
           // overLayClassName="modal-overlay"
           className="modal-style"
         >
-
               <h2>Criando novo cadastro</h2>
-              <hr />
+              <hr/>
               <p></p>
                 <label>Informe seu email:</label>
                 <br></br>
@@ -86,19 +70,18 @@ Modal.setAppElement("#root");
                 <label>Confirmar senha:</label>
                 <br></br>
                 <input type="password" class="input-basic" id="password-login-input"/>
-              <div class="row">
-                <br></br>
-                  <button class="btn button-basic" onClick={closeModal}>Cancelar</button>
-                  <br></br>
-                  <br></br>
-                  <button class="btn button-basic">Registrar</button>
+              <p></p>
 
+              <div class="mb-3">
+                <div>
+                    <button class="btn button-basic" onClick={closeModal}>Cancelar</button>                   
+                    <button class="btn button-basic">Registrar</button>
+                </div>
               </div>
 
         </Modal>
         </div>
       </body>
-    </div>
   )
 
 }
