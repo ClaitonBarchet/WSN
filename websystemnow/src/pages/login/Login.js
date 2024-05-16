be9ebc3c79144517eadd4d1ab38ebd6cc759f694
ClaitonBarchet <claitonbarchet@live.com>
2024-05-16 15:44:04 -0300

    ajuste validação

--- websystemnow/src/pages/login/Login.js
+++ websystemnow/src/pages/login/Login.js
@@ -7,13 +7,39 @@ Modal.setAppElement("#root");
 
 const Login = () => {
     const [modalIsOpen, setIsOpen] = useState(false);
-    const [email, setEmail]= useState("")
-    // METODO CADASTRAR
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    
+    // METODO CADASTRAR - MODAL
     const cadastrar = async (e) => {
       e.preventDefault()
-      // if(email)
 
-      alert("teste modal cadastro")
+      if(email == ("")){
+        alert("Informe um email válido!");
+        return;
+      }
+
+      if(password == ("")){
+        alert("Informe uma senha!");
+        return;
+      }
+
+      if(confirmPassword == ("")){
+        alert("Confirme a senha!");
+        return;
+      }
+
+      if(password !== confirmPassword){
+        alert("As senhas precisam ser iguais!");
+        return;
+      }
+
+      if(password.length < 6) {
+        alert("A senha precisa ter no mínimo 6 digitos.");
+        return;
+      }
+      
 
     }
 
@@ -70,26 +96,48 @@ const Login = () => {
 {/* MODAL */}
           <div className = "modal"> 
           {/* https://reactcommunity.org/react-modal/ */}
-            <Modal 
-              isOpen={modalIsOpen}
-              onRequestClose={closeModal}
-              className="modal-style"
-            >
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="modal-style">
               <FormGroup>
                 <h2>Criando novo cadastro</h2>
                 <hr/>
                   <p></p>
+
+                    {/* EMAIL */}
                     <label>Informe seu email:</label>
                     <br></br>
-                    <input type="email" class="input-modal" name="email"/>
+                    <input type="email" 
+                    name="email" 
+                    style={{textTransform:"uppercase"}} 
+                    class="input-modal" 
+                    value={email} 
+                    required 
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+
                     <br></br>
+
+                    {/* SENHA */}
                     <label>Senha:</label>
                     <br></br>
-                    <input type="password" class="input-modal" id="password-login-input"/>
+                    <input type="password"
+                    name = "password"
+                    class="input-modal" 
+                    value={password}
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    {/* CONFIRMAR SENHA */}
+                    <label>Confirme a senha:</label>
                     <br></br>
-                    <label>Confirmar senha:</label>
-                    <br></br>
-                    <input type="password" class="input-modal" id="password-login-input"/>
+                    <input type="password"
+                    name = "confirmPassword"
+                    class="input-modal" 
+                    value={confirmPassword}
+                    required
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+
                   <div>
                     <div>
                         <button class="btn button-modal" onClick={closeModal}>Cancelar</button>                   
